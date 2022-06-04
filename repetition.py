2adffb07c39a0d5292a04ac8538d57266e581bd3
koxx4 <piotr815@poczta.fm>
2022-06-04 09:23:16 +0200

    Repetition code

---
+++ repetition.py
@@ -0,0 +1,26 @@
+from bitarray import bitarray
+from util import bits_from_bytes
+
+
+def encode_repetition(data: bytes, repCount: int = 2) -> bytes:
+    bits = bits_from_bytes(data)
+    encodedSize = len(bits) * repCount
+
+    bitsEncoded = bitarray()
+
+    for bit in bits:
+        for j in range(0, repCount):
+            bitsEncoded.append(bit)
+    
+    return bitsEncoded.tobytes()
+    
+
+def validate_encoded_repetition(encodedData: bytes, repCount: int = 2) -> bool:
+    bits = bits_from_bytes(encodedData)
+
+    for i in range(0, len(bits), repCount):
+        for j in range(0, repCount):
+            if(bits[i] != bits[j]):
+                return False
+    
+    return True
